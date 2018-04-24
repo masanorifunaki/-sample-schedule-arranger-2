53ade2c9798402ffd19f9a6ee45921fffe7f645b
舟木正憲 <funakimfunaki@gmail.com>
2018-04-24 09:01:25 +0900

    first commit

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let helmet = require('helmet');
+let session = require('express-session');
+let passport = require('passport');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+let GitHubStrategy = require('passport-github2');
+let GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+let GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
-var app = express();
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+
+});
+
+let index = require('./routes/index');
+let users = require('./routes/users');
+
+let app = express();
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,7 +45,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
